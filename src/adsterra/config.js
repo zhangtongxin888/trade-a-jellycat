6034534e170c185d60cb36e9d1c72437ef37af29
zhangtongxin888 <zhangtongxin12@gmail.com>
2026-08-20 07:26:37 +0800

    feat: add Adsterra placements

---
+++ src/adsterra/config.js
@@ -0,0 +1,29 @@
+// adsterra-code-handoff:mpa-config
+// Domain: tradeajellycatguide.wiki
+export const adsterraConfig = {
+  "adsterraNeedsGlobalFallback": 1,
+  "adsterraBannerId": "21bb162003d7c39f1b92d51dc6b48929",
+  "adsterraBanner300x250Key": "21bb162003d7c39f1b92d51dc6b48929",
+  "adsterraBanner300x250ScriptUrl": "https://www.highperformanceformat.com/21bb162003d7c39f1b92d51dc6b48929/invoke.js",
+  "adsterraBanner320x50Key": "191a6397c648e7f89336555a287a96f5",
+  "adsterraBanner320x50ScriptUrl": "https://www.highperformanceformat.com/191a6397c648e7f89336555a287a96f5/invoke.js",
+  "adsterraBanner728x90Key": "264ccaf27b40386f234157609b545e47",
+  "adsterraBanner728x90ScriptUrl": "https://www.highperformanceformat.com/264ccaf27b40386f234157609b545e47/invoke.js",
+  "adsterraBanner468x60Key": "2d7ccc7ff048ffcecb4ada301f6ce210",
+  "adsterraBanner468x60ScriptUrl": "https://www.highperformanceformat.com/2d7ccc7ff048ffcecb4ada301f6ce210/invoke.js",
+  "adsterraBanner160x300Key": "240d8c2a8c45d7753fb8c64b23212f82",
+  "adsterraBanner160x300ScriptUrl": "https://www.highperformanceformat.com/240d8c2a8c45d7753fb8c64b23212f82/invoke.js",
+  "adsterraBanner160x600Key": "88f5ac1928f9998c2de961028b89f1e7",
+  "adsterraBanner160x600ScriptUrl": "https://www.highperformanceformat.com/88f5ac1928f9998c2de961028b89f1e7/invoke.js",
+  "adsterraNative1Id": "container-a50d8d76e3e9e524cd0fb650af01c44c",
+  "adsterraNative1ScriptUrl": "https://pl30931115.effectivecpmnetwork.com/a50d8d76e3e9e524cd0fb650af01c44c/invoke.js",
+  "adsterraSmartLinkUrl": "https://www.effectivecpmnetwork.com/ibxjc6qbc?key=29e9914525f48e847e65c482d0aaf672",
+  "adsterraPopunderScriptUrl": "https://pl30931111.effectivecpmnetwork.com/92/3c/0b/923c0b9bf34ca84f889802769bde004f.js",
+  "adsterraSocialBarScriptUrl": "https://pl30931112.effectivecpmnetwork.com/8d/6b/8f/8d6b8f639c32370a653bef325b115c48.js",
+  "adsterraLeaderboardId": "264ccaf27b40386f234157609b545e47",
+  "adsterraEnablePopunder": false,
+  "adsterraEnableSocialBar": false,
+  "adsterraEnableStickyRail": false,
+  "adsterraPopunderDelayMs": 30000,
+  "adsterraPopunderMinPageViews": 2
+};
